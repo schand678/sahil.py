e1cd559af1cfc885f4b879b4aabb870bb68f6c93
schand678 <sahilchand055@gmail.com>
2024-12-09 18:23:25 -0700

    Update 2_Dashboard.py

--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -4,13 +4,7 @@ import pandas as pd
 # Title of the app
 st.title("Vehicle Make Clustering: Unlocking Sales Insights")
 
-# URL of the CSV file in your GitHub repository
+data = pd.read_csv("clusterst__rows.csv")
 
+st.data.head(10)
 
-try:
-    # Read the CSV file from GitHub
-    data = pd.read_csv("clusterst__rows.csv")
-
-    st.data.head(10)
-except Exception as e:
-    st.error(f"Error loading data: {e}")
