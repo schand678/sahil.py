f6d0830a971104333874e7a9257ba9cde97d4a4b
schand678 <sahilchand055@gmail.com>
2024-12-09 18:28:47 -0700

    Update 2_Dashboard.py

--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -1,10 +1,4 @@
 import streamlit as st
 import pandas as pd
 
-# Title of the app
-st.title("Vehicle Make Clustering: Unlocking Sales Insights")
-
-data = pd.read_csv("clusterst__rows.csv")
-
-st.data.head(10)
-
+st.image("")
