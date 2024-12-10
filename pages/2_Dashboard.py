e0c22530b32a7a00acd8af3fe2cba5f4a62bb6c2
schand678 <sahilchand055@gmail.com>
2024-12-09 18:22:55 -0700

    Update 2_Dashboard.py

--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -10,9 +10,7 @@ st.title("Vehicle Make Clustering: Unlocking Sales Insights")
 try:
     # Read the CSV file from GitHub
     data = pd.read_csv("clusterst__rows.csv")
-    
-    # Display the first 10 rows
-    st.subheader("First 10 Rows of the Dataset")
-    st.write(data.head(10))
+
+    st.data.head(10)
 except Exception as e:
     st.error(f"Error loading data: {e}")
